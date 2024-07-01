f6bc6dd17c2e9f79bea686e7b344007b07208b66
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-07-01 13:59:13 +0600

    commit

--- main.go
+++ main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	_ "github.com/jackc/pgx/v5"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 }
-
-// version: "2"
-// sql:
-//   - engine: "postgresql"
-//     queries: "query.sql"
-//     schema: "schema.sql"
-//     gen:
-//       go:
-//         package: "tutorial"
-//         out: "tutorial"
-//         sql_package: "pgx/v5"
